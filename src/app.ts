6c007eaee95783348de6b9f789f3cbbc854216ad
freykill <freykill1999@gmail.com>
2024-08-13 11:10:22 -0500

    modificaciones de cors

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,18 @@ import express from "express";
 import authRoutes from './routes/authRoutes';
 import clientesRoutes from './routes/clientesRoutes'
 import serviciosRoutes from './routes/serviciosRoutes'
+import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5174', // Permite solo este origen
+    // Si deseas permitir todos los orígenes, usa:
+    // origin: '*'
+  }));
 
 // Routes PRINCIPALES
 //REGISTRO, LOGIN
